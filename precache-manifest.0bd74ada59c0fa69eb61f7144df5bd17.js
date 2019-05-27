a91c872aa4ad1e45a668cb08c5fd29f1e4abf232
mehboobsamnani <mehboob.samnani@gmail.com>
2019-05-28 01:06:49 +0400

    Updates

---
+++ precache-manifest.0bd74ada59c0fa69eb61f7144df5bd17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd291a52b62fb44b40e78b6f94d63cb6",
+    "url": "/chalhoub/static/media/left-arrow.dd291a52.svg"
+  },
+  {
+    "revision": "46832e296cda51111c1b",
+    "url": "/chalhoub/static/js/runtime~main.46832e29.js"
+  },
+  {
+    "revision": "c25d60241f448d7f5e73",
+    "url": "/chalhoub/static/js/main.c25d6024.chunk.js"
+  },
+  {
+    "revision": "5d1630978e8a9ac2dfe9",
+    "url": "/chalhoub/static/js/2.5d163097.chunk.js"
+  },
+  {
+    "revision": "c25d60241f448d7f5e73",
+    "url": "/chalhoub/static/css/main.26b74b57.chunk.css"
+  },
+  {
+    "revision": "5d1630978e8a9ac2dfe9",
+    "url": "/chalhoub/static/css/2.dd829d13.chunk.css"
+  },
+  {
+    "revision": "afba35ad59468bbdbf14cb89552af414",
+    "url": "/chalhoub/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/chalhoub/precache-manifest.0bd74ada59c0fa69eb61f7144df5bd17.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/chalhoub/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
